fbbb633ebbd220750db3808e813d9d4509fcdc32
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-05-17 20:10:09 +0100

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Hello World",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
 
+let poemFormElement = document.querySelector("#search-form");
+poemFormElement.addEventListener("submit", generatePoem);
