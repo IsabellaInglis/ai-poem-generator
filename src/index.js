3b1429624ca0e2c9dc40bf3441ffb37fe6528a34
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-05-26 10:54:11 +0100

    Amended css styling

--- src/index.js
+++ src/index.js
@@ -10,10 +10,14 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
+
+  let poemSection = document.querySelector("#poem");
+  poemSection.classList.remove("hide");
+
   let topic = document.querySelector("#poem-topic");
   let apiKey = `dc6760cf7088c245e5a42a646bco203t`;
   let context = `You are an intelligent AI assistant that has access to a full dictionary or rhyming words and poem examples, which allows you to come up with many creative poem ideas`;
-  let prompt = `Write me a short poem about ${topic.value},that is 2-4 lines long. The poem must be provided in the following HTML format: <div>Poem line 1<br>Poem line 2</div>`;
+  let prompt = `Write me a short poem about ${topic.value},that is 2-4 lines long. The poem must be provided in the following HTML format: "<div>Poem line 1<br>Poem line 2<br>Poem line 3<br>Poem line 4</div>`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
