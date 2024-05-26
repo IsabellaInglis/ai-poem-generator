c23a90ea05078ee7121b5f83c10a6f614a219d6e
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-05-26 11:39:10 +0100

    Added length selection

--- src/index.js
+++ src/index.js
@@ -13,12 +13,27 @@ function generatePoem(event) {
 
   let poemSection = document.querySelector("#poem");
   poemSection.classList.remove("hide");
+  poemSection.innerHTML = "Generating poem...";
 
   let topic = document.querySelector("#poem-topic");
   let apiKey = `dc6760cf7088c245e5a42a646bco203t`;
   let context = `You are an intelligent AI assistant that has access to a full dictionary or rhyming words and poem examples, which allows you to come up with many creative poem ideas`;
-  let prompt = `Write me a short poem about ${topic.value},that is 2-4 lines long. The poem must be provided in the following HTML format: "<div>Poem line 1<br>Poem line 2<br>Poem line 3<br>Poem line 4</div>`;
 
+  const shortPoem = document.getElementById("short");
+  const mediumPoem = document.getElementById("medium");
+  const longPoem = document.getElementById("long");
+
+  let selectedLength;
+
+  if (shortPoem.checked) {
+    selectedLength = "2-4 lines";
+  } else if (mediumPoem.checked) {
+    selectedLength = "6-8 lines";
+  } else if (longPoem.checked) {
+    selectedLength = "10-12 lines";
+  }
+
+  let prompt = `Write me a short poem about ${topic.value},that is ${selectedLength} long. The poem must be provided in the following HTML format: "<div>Poem line 1<br>Poem line 2<br>Poem line 3<br>Poem line 4</div>" and so on.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
